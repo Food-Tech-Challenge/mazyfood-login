ec30a6901e85a75eab5b6a758e939c0e39e75049
matheusdeoliveira <matheusoliveira.info@gmail.com>
2025-03-23 15:39:19 -0300

    feat: adding lambda layers

---
+++ terraform/lambda_layers.tf
@@ -0,0 +1,11 @@
+resource "aws_lambda_layer_version" "boto3_layer" {
+  layer_name = "boto3"
+  filename = "../src/layers/boto3.zip"
+  compatible_runtimes = ["python3.12"]
+}
+
+resource "aws_lambda_layer_version" "psycopg2_layer" {
+  layer_name = "psycopg2"
+  filename = "../src/layers/psycopg2-binary.zip"
+  compatible_runtimes = ["python3.12"]
+}
