e6fcf7ddc2376253b369233de5edbd0bc8576c51
matheusdeoliveira <matheusoliveira.info@gmail.com>
2025-03-23 16:56:48 -0300

    feat: adding lambda vpc to connect to database

--- terraform/data.tf
+++ terraform/data.tf
@@ -7,3 +7,29 @@ data "archive_file" "python_lambda_package" {
 data "aws_iam_role" "lab_role" {
   name = "LabRole"
 }
+
+data "aws_vpc" "selected_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["mazyfood-vpc"]
+  }
+}
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.selected_vpc.id]
+  }
+
+  filter {
+    name   = "map-public-ip-on-launch"
+    values = ["false"]
+  }
+}
+
+data aws_security_group "selected_security_group" {
+  filter {
+    name = "tag:Name"
+    values = ["mazyfood-security-group-db"]
+  }
+}
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,13 +1,18 @@
 resource "aws_lambda_function" "mazyfood_auth_lambda" {
-  function_name = "mazyfood_auth_lambda_function"
+  function_name = "mazyfood_auth_lambda"
   description   = "MazyFood Auth Lambda Function"
   architectures = ["x86_64"]
   filename = "lambda_function.zip"
   source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
   handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
+  runtime       = "python3.12"
   role          = data.aws_iam_role.lab_role.arn
   layers = [aws_lambda_layer_version.boto3_layer.arn, aws_lambda_layer_version.psycopg2_layer.arn]
+
+  vpc_config {
+    subnet_ids         = toset(data.aws_subnets.private.ids)
+    security_group_ids = [data.aws_security_group.selected_security_group.id]
+  }
 
   environment {
     variables = {
